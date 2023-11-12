1d19e698f842d964198e5af75bcaabacd6c7ea48
Bogzbogz08 <149877879+Bogzbogz08@users.noreply.github.com>
2023-11-12 11:03:05 +0800

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,113 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Why is listening such an important ability to have?",
+    answer: "To avoid miscommunications",
+    options: [
+      "To avoid miscommunications",
+      "To listen to our favorite music",
+      "To become a better speaker",
+      "To become more interested in a topic"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What is the reason on why we listen?",
+    answer: "To learn new information",
+    options: [
+      "To entertain ourselves",
+      "To become a better speaker",
+      "To learn new information",
+      "To remember what we learned before"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What type of textual aid is in the form of rows and columns?",
+    answer: "Table",
+    options: [
+      "Table",
+      "Graph",
+      "Title",
+      "Structure"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Which is not part of the setting of the story?",
+    answer: "Conflict",
+    options: [
+      "The place",
+      "Conflict",
+      "The time",
+      "The weather"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What is chronological order?",
+    answer: "The sequence of the story starting from beginning to end",
+    options: [
+      "Hint of the future hidden in the beginning of the story",
+      "The highest point of the story",
+      "The lowest point of the story",
+      "The sequence of the story starting from beginning to end"
+    ]
+  },
+     {
+     numb: 6,
+     question: "What is the dispute between opposing forces?",
+     answer: "Conflict",
+     options: [
+       "War",
+       "Destruction",
+       "Conflict",
+       "Climax"
+     ]
+   },
+   {
+    numb: 7,
+    question: "What is the highest point of the story?",
+    answer: "Climax",
+    options: [
+      "Conflict",
+      "Foreshadowing",
+      "Ending",
+      "Climax"
+    ]
+  },
+  {
+    numb: 8,
+    question: "What is foreshadowing?",
+    answer: "Hint of the future hidden in the beginning of the story",
+    options: [
+      "The conclusion of the story",
+      "Hint of the future hidden in the beginning of the story",
+      "A scary story",
+      "A summary of what happened in the past"
+    ]
+  },
+  {
+    numb: 9,
+    question: "What is the tool that can help readers understand the meaning of the text?",
+    answer: "Textual Aids",
+    options: [
+      "Textual Aids",
+      "Google translate",
+      "Encyclopedia",
+      "ChatGPT"
+    ]
+  },
+  {
+    numb: 10,
+    question: "What type of graphic organizer depicts the process of a certain topic or idea?",
+    answer: "Flow chart",
+    options: [
+      "Fishbone diagram",
+      "Concept Map",
+      "Venn diagram",
+      "Flow chart"
+    ]
+  },
+];
